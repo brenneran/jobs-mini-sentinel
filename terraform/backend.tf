40cb759834846bb876d625ea6b1ea00ee2f208ac
Andrey Brenner <a.brenner@aligntech.com>
2025-08-20 17:12:23 +0300

    fixing the s3 bucket of the backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "jobs-mini-sentinel-state-bucket"
+    bucket       = "jobs-mini-sentinel"
     key          = "align-jobs-lambda/terraform.tfstate"
     region       = "us-west-2"
     use_lockfile = true
