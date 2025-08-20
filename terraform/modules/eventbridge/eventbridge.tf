98335fc28cc271476b1801b4303e1918c9503603
Andrey Brenner <a.brenner@aligntech.com>
2025-08-20 17:08:41 +0300

    missing write terraform for eventbridge

--- terraform/modules/eventbridge/eventbridge.tf
+++ terraform/modules/eventbridge/eventbridge.tf
@@ -1,7 +1,18 @@
-variable "lambda_function_name" {
-  type = string
+resource "aws_cloudwatch_event_rule" "schedule" {
+  name                = "${var.lambda_function_name}-rule"
+  schedule_expression = var.schedule_expression
 }
 
-variable "schedule_expression" {
-  type = string
+resource "aws_cloudwatch_event_target" "lambda" {
+  rule      = aws_cloudwatch_event_rule.schedule.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.this.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.schedule.arn
 }
---
+++ terraform/modules/eventbridge/variables.tf
@@ -0,0 +1,7 @@
+variable "lambda_function_name" {
+  type = string
+}
+
+variable "schedule_expression" {
+  type = string
+}
